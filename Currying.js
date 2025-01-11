7f8cdb052da5519b28d390f9909f3bb28f4d6eb3
dhinesh.ksd <dhinaksd@gmail.com>
2025-01-11 15:24:40 +0530

    dev: currying example

--- Currying.js
+++ Currying.js
@@ -64,6 +64,21 @@ const sum = (a) => {
 const sumResult = sum(1)(2)(3)(4)(5)();
 console.log(sumResult); // 15
 
+// sum(1,2,...,n)(3,4,...,n)...(n)()
+
+// we just reduce the arguments into a single argument for
+// a single call! Now it's just like the previous problem!
+function sum(...args) {
+  let a = args.reduce((a, b) => a + b, 0);
+  return function (...args) {
+    let b = args.reduce((a, b) => a + b, 0);
+    if (b) {
+      return sum(a + b);
+    }
+    return a;
+  };
+}
+
 // 💡converting multply(a,b,c,d) into multply(a)(b)(c)(d)
 
 const multiply = (a, b, c, d) => {
@@ -73,14 +88,23 @@ const multiply = (a, b, c, d) => {
 const value = multiply(1, 2, 3, 4);
 console.log(value); // 24
 
+// The curry function is a higher-order function that transforms a regular function into a curried version.
+// Currying allows a function to be called in a sequence of calls, each with a single argument, instead of passing all arguments at once.
+
 function curry(callback) {
   // callback refers 👆 to multiply function here
 
   return function curried(...args) {
     //  👇 If no. of args >= args of multiply func, then call multiply function and get the result.
     if (args.length >= callback.length) {
+      // Purpose: This checks if the number of arguments passed to curried is equal to or greater than the number of arguments expected by the callback function.
+      // callback.length: This gives the number of parameters the original function (multiply) expects.
+      // If true: The callback function is called with the accumulated arguments using the spread operator (...args), and the result is returned.
       return callback(...args);
     } else {
+      // Purpose: If fewer arguments are provided than needed, it returns a new function that accepts more arguments.
+      // Closure: The inner anonymous function has access to the args variable from curried because of JavaScript closures. This allows the function to remember the previously passed arguments.
+      // Recursive Call: The curried function is called recursively with the combined arguments (...args, ...nextArg). This continues until the number of arguments is sufficient to call the original function.
       // 👇 inner anonymous function has access to curried func args variable due to closures.
       return (...nextArg) => {
         //  👇 recursively calling curried func until no. of args >= args of multiply func.
